032ba4dee2d111b77a743a321a986ab1ce163a28
momenashrafff <momen.elkhouli@student.giu-uni.de>
2025-04-05 21:47:54 +0200

    added icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Momen Elkhouli',
   description: 'Momen Elkhouli\'s Portfolio',
+  icons: {
+    icon: '/favicon.svg',
+  },
 }
 
 export default function RootLayout({
